00a67674392aa1b6f772308cf77c9cf2bd90c048
Simran Inamdar <125954439+simraninamdar09@users.noreply.github.com>
2024-09-11 10:08:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns 
 import numpy as np
-import os,re
 import streamlit as st
 import warnings
 from sklearn import preprocessing
